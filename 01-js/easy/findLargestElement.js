e8d424c6002925875dfab7380ee6d87b4d92d27e
sandeepgottipati <snchowdary7@gmail.com>
2023-12-22 23:17:52 -0600

    implemented findLargestElement-easy

--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -6,7 +6,11 @@
 */
 
 function findLargestElement(numbers) {
-    
+  if (numbers.length === 1) return numbers[0];
+  if (numbers.length === 0) return;
+  let max = Math.max(...numbers);
+  return max;
 }
 
 module.exports = findLargestElement;
+
