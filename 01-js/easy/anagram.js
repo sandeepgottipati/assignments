907930a0aeffc9bc656f9943ea2fd8ed9ad86dfd
sandeepgottipati <snchowdary7@gmail.com>
2023-12-22 22:21:03 -0600

    implemented anagram and added jest

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,15 @@
 */
 
 function isAnagram(str1, str2) {
+  if (str1.length === 0 && str2.length === 0) return true;
+  if (str1.length === 0 || str2.length === 0) return false;
+  if (str1.length !== str2.length) return false;
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
 
+  let sortedStringOne = str1.split("").sort().join("");
+  let sortedStringTwo = str2.split("").sort().join("");
+  return sortedStringOne === sortedStringTwo;
 }
-
+console.log(isAnagram("sar", "rai"));
 module.exports = isAnagram;
