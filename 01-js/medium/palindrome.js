e544e625bb8e1c8917f8b32de32845560ec3eb00
sandeepgottipati <snchowdary7@gmail.com>
2023-12-24 09:27:53 -0600

    solved palindrome.js

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -3,6 +3,22 @@
   Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
 */
 
-function isPalindrome(str) {}
+function isPalindrome(str) {
+  if (str.length === 1) { return true; }
+
+  let modifiedStr = str.replace(/[^a-z]/gi, "");
+  modifiedStr = modifiedStr.toLowerCase();
+  console.log(modifiedStr);
+
+  const modifiedArr = new Array(...modifiedStr);
+  console.log(modifiedArr)
+  const result = modifiedArr.reverse().join('');
+  console.log(result);
+  return result === modifiedStr;
+
+
+}
+console.log(isPalindrome("openai").toString())
+
 
 module.exports = isPalindrome;
