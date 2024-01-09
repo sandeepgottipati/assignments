01ea9f7bba63a8c9f30ab116ba494992514f7aa5
sandeepgottipati <snchowdary7@gmail.com>
2024-01-08 19:24:44 -0600

    implemented week-2

---
+++ week-2/01-async-js/easy/readfile.js
@@ -0,0 +1,18 @@
+var users = [
+  {
+    name: "sandeep",
+    kidneys: [
+      {
+        healthy: false,
+      },
+    ],
+  },
+  {
+    name: "gottipati",
+    kidneys: [
+      {
+        healthy: false,
+      },
+    ],
+  },
+];
